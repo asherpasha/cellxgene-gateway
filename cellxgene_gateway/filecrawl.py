6565177f75d114437f4f8a42ce70e8c6fd557dc0
asherpasha <asher.pasha@utoronto.ca>
2026-04-10 14:47:28 -0400

    Minor changes.

--- cellxgene_gateway/filecrawl.py
+++ cellxgene_gateway/filecrawl.py
@@ -63,6 +63,4 @@ def render_item_tree(item_tree, item_source):
 
 def render_item_source(item_source, filter=None):
     item_tree = item_source.list_items(filter)
-    filterpart = "" if filter is None else ":" + filter
-    heading = f"<h6><a href='/filecrawl.html?source={urllib.parse.quote_plus(item_source.name)}'>{item_source.name}</a>{filterpart}</h6>"
-    return heading + render_item_tree(item_tree, item_source)
+    return render_item_tree(item_tree, item_source)
